412fddb8c277487c3afa7396ca6924c30582c29b
Rahul Yadav <rahulcs989@gmail.com>
2024-08-10 14:56:44 +0530

    signin.component.jsx updated

--- src/assets/components/signInSignUp/signIn.components.jsx
+++ src/assets/components/signInSignUp/signIn.components.jsx
@@ -1,99 +1,14 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import {
-  MDBInput,
-=======
 import React, { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
   MDBInput,
   MDBTypography,
->>>>>>> main
   MDBCol,
   MDBRow,
   MDBCheckbox,
   MDBBtn,
   MDBIcon,
-<<<<<<< HEAD
-  MDBContainer
-} from 'mdb-react-ui-kit';
-import './signIn.components.css';
-import { useNavigate } from 'react-router-dom';
-import Footer from '../footer/footer.components';
-// import SignInDashboard from './signInDashboard/signInDashboard.components';
-
-export default function SignIn({data}) {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
-
-    function func(){
-        if(localStorage.getItem(email)){
-            data(email);
-            navigate('/signInDashboard');
-        }
-        else{
-            alert("Wrong email or password");
-        }
-    }
-  return (
-    <>
-    <form className='pb-5'>
-        <MDBContainer className='p-5 mt-5 signIn__form'>
-            <MDBInput className='mb-4' type='email' id='form2Example1' onChange={(e)=>{setEmail(e.target.value)}} label='Email address' />
-            <MDBInput className='mb-4' type='password' id='form2Example2' onChange={(e)=>{setPassword(e.target.value)}} label='Password' />
-
-            <MDBRow className='mb-4'>
-                <MDBCol className='d-flex justify-content-center'>
-                <MDBCheckbox id='form2Example3' label='Remember me' flexCheckDefault />
-                </MDBCol>
-            </MDBRow>
-            <MDBRow className='mb-4'>
-            <MDBCol className='d-flex justify-content-center signIn__forgotText'>
-                <a href='#!'>Forgot password?</a>
-                </MDBCol>
-            </MDBRow>
-
-            <MDBBtn type='submit' className='mb-4 signIn__button' block onClick={()=>{
-                // navigate('/signInDashboard')
-                func();
-            }}>
-                Sign in
-            </MDBBtn>
-
-            <div className='text-center signIn__forgotText'>
-                <p>
-                Not a member? <a href='#!' onClick={()=>{
-                    navigate('/signUp');
-                }}>Register</a>
-                </p>
-                <p>or sign In with:</p>
-
-                <MDBBtn floating color="secondary" className='mx-1 signIn__icon'>
-                <MDBIcon fab icon='facebook-f' />
-                </MDBBtn>
-
-                <MDBBtn floating color="secondary" className='mx-1 signIn__icon'>
-                <MDBIcon fab icon='google' />
-                </MDBBtn>
-
-                <MDBBtn floating color="secondary" className='mx-1 signIn__icon'>
-                <MDBIcon fab icon='twitter' />
-                </MDBBtn>
-
-                <MDBBtn floating color="secondary" className='mx-1 signIn__icon'>
-                <MDBIcon fab icon='github' />
-                </MDBBtn>
-            </div>
-        </MDBContainer>
-    </form>
-    <Footer/>
-    </>
-  );
-}
-=======
   MDBContainer,
 } from "mdb-react-ui-kit";
 import "./signIn.components.css";
@@ -103,11 +18,8 @@ import axios from "axios";
 
 export default function SignIn() {
   const navigate = useNavigate();
-  // const [fetchData, setFetchData] = useState([]);
-  // console.log("fetch the data", fetchData);
   const [role, setRole] = useState("");
   const [email, setEmail] = useState("");
-  console.log("email-", email);
   const [password, setPassword] = useState("");
 
   const userLogin = async () => {
@@ -115,8 +27,13 @@ export default function SignIn() {
       let endpoint = "";
       if (role === "tourist") {
         endpoint = "http://localhost:8080/tourist";
-      } else {
+      } else if (role === "guide") {
         endpoint = "http://localhost:8080/guide";
+      }
+
+      if (!endpoint) {
+        console.error("Please select a role.");
+        return;
       }
 
       const res = await axios.get(endpoint, {
@@ -125,19 +42,13 @@ export default function SignIn() {
           password: password,
         },
       });
-      console.log(res.data);
-
-      // setFetchData(res.data);
 
       if (res.status === 200) {
         const userData = res.data[0];
-        if (userData.password === password) {
-          if (userData.role === role) {
-            sessionStorage.setItem("email", email);
-            sessionStorage.setItem("role", role);
-            navigate(`/signInDashboard/${role}/${email}`);
-            console.log("login successfully");
-          }
+        if (userData.password === password && userData.role === role) {
+          sessionStorage.setItem("email", email);
+          sessionStorage.setItem("role", role);
+          navigate(`/signInDashboard/${role}/${email}`);
         } else {
           console.error("Enter correct credentials");
         }
@@ -148,14 +59,14 @@ export default function SignIn() {
       console.error("Login error: " + err.message);
     }
   };
+
   return (
     <>
       <form className="pb-5">
         <MDBContainer className="p-5 mt-5 signIn__form">
-          {/* <MDBSelect optionData={[{ text: "One", value: 1 }]} /> */}
           <MDBRow className="mb-2">
             <MDBCol>
-              <MDBTypography>Are you Tourist or Guide?. </MDBTypography>
+              <MDBTypography>Are you Tourist or Guide?</MDBTypography>
             </MDBCol>
             <MDBCol className="">
               <select
@@ -175,10 +86,7 @@ export default function SignIn() {
             type="email"
             name="email"
             value={email}
-            onChange={(e) => {
-              console.log("email : - ", e.target.value);
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => setEmail(e.target.value)}
             label="Email address"
           />
           <MDBInput
@@ -186,9 +94,7 @@ export default function SignIn() {
             type="password"
             name="password"
             value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
+            onChange={(e) => setPassword(e.target.value)}
             label="Password"
           />
 
@@ -212,10 +118,8 @@ export default function SignIn() {
             className="mb-4 signIn__button"
             block
             onClick={userLogin}
-            navigate="/signInDashboard"
           >
             Sign in
-            {/* <Link to="/signInDashboard"></Link> */}
           </MDBBtn>
 
           <div className="text-center signIn__forgotText">
@@ -255,4 +159,3 @@ export default function SignIn() {
     </>
   );
 }
->>>>>>> main
